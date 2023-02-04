841e4c0caf08336a351643385029cacb230c7622
Herbert Knaf <58120773+HerbIzzle@users.noreply.github.com>
2023-02-04 19:35:08 +0100

    tests added

--- leiwander-wecker/src/app/alarm/alarm.component.spec.ts
+++ leiwander-wecker/src/app/alarm/alarm.component.spec.ts
@@ -1,23 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlarmComponent } from './alarm.component';
 
 describe('AlarmComponent', () => {
   let component: AlarmComponent;
-  let fixture: ComponentFixture<AlarmComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AlarmComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(AlarmComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new AlarmComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 24 hours and 60 minutes', () => {
+    expect(component.hours.length).toBe(24);
+    expect(component.minutes.length).toBe(60);
+  });
+
+  it('should add an alarm', () => {
+    component.selectedHour = '02';
+    component.selectedMinute = '30';
+    component.addAlarm();
+    expect(component.alarm).toEqual({ hour: '02', minute: '30', snooze: false });
+  });
+
+  it('should remove an alarm', () => {
+    component.stopAlarm();
+    expect(component.alarm).toEqual({ hour: '', minute: '', snooze: false });
+  });
+
+  it('should toggle the sound', () => {
+    component.soundEnabled = false;
+    component.toggleSound();
+    expect(component.soundEnabled).toBeTrue();
+  });
+
+  it('should snooze', () => {
+    component.snooze();
+    expect(component.snoozeMsg).toBeTrue();
   });
 });
